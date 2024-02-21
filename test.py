a219cb323cd1dcbfcbb784eecdb53e8e2ab5d352
kerinpatel19 <153475039+kerinpatel19@users.noreply.github.com>
2024-02-20 21:36:42 -0600

    added sub categoryies

--- test.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-class Menu(ttk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-        
-        ttk.Label(self,background="#8e9bb9").pack(expand= True, fill='both')
-  
